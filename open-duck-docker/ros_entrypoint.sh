f456c5f239e53d0bce3b206bafd6e00677779350
Jaime <jaime@element.aero>
2025-05-29 00:22:36 -0600

    add ROS docker compose files

---
+++ open-duck-docker/ros_entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# source the ROS 2 setup
+source /opt/ros/jazzy/setup.bash
+echo "Sourcing bdx_ws"
+source /bdx_ws/install/setup.bash
+# then exec whatever the user passed in
+exec "$@"
